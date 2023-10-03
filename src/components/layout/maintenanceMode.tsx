3c048b1df943d37a35eab868c382513655e45d85
Davut Türüğ <davut.0773@gmail.com>
2023-10-04 01:49:16 +0300

    fix: translation of maintenance mode

--- src/components/layout/maintenanceMode.tsx
+++ src/components/layout/maintenanceMode.tsx
@@ -1,8 +1,10 @@
 import { Stack, Typography } from "@mui/material";
 import { useAppSelector } from "../../store/hooks";
+import { useTranslate } from "../../hooks/useTranslate";
 
 export const MaintenanceMode = () => {
   const globalState = useAppSelector((state) => state.global);
+  const translate = useTranslate();
   return globalState.inMaintenance ? (
     <Stack
       gap={10}
@@ -21,9 +23,12 @@ export const MaintenanceMode = () => {
     >
       <img width="300" src="./images/auth/maintain.svg" alt="desktop" />
       <Stack alignItems="center" gap={2}>
-        <Typography variant="h1"> We'll be right back!</Typography>
+        <Typography variant="h1">
+          {" "}
+          {translate("component.maintenance_mode.title")}{" "}
+        </Typography>
         <Typography variant="h3">
-          We are currently in maintenance mode. We will be back online soon!
+          {translate("component.maintenance_mode.message")}
         </Typography>
       </Stack>
     </Stack>
