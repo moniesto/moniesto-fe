2725e7ff7f659b9f7a12a594efc5daf3db2bc3d5
Davut Türüğ <davut.0773@gmail.com>
2023-10-04 11:57:18 +0300

    syle: maintenance ui by mode

--- src/components/layout/maintenanceMode.tsx
+++ src/components/layout/maintenanceMode.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from "@mui/material";
+import { Card, Stack, Typography } from "@mui/material";
 import { useAppSelector } from "../../store/hooks";
 import { useTranslate } from "../../hooks/useTranslate";
 
@@ -7,30 +7,41 @@ export const MaintenanceMode = () => {
   const translate = useTranslate();
   return globalState.inMaintenance ? (
     <Stack
-      gap={10}
       alignItems="center"
       sx={{
         position: "fixed",
         top: 0,
+        left: 0,
+        right: 0,
+        margin: "10px",
+        width: "calc(100% - 20px)",
         zIndex: 2,
-        width: "100%",
         height: "100%",
         transition: "all 0.4s ease",
-        backdropFilter: "blur(30px)",
+        backdropFilter: "blur(20px)",
         display: "flex",
         justifyContent: "center",
       }}
     >
-      <img width="300" src="./images/auth/maintain.svg" alt="desktop" />
-      <Stack alignItems="center" gap={2}>
-        <Typography variant="h1">
-          {" "}
-          {translate("component.maintenance_mode.title")}{" "}
-        </Typography>
-        <Typography variant="h3">
-          {translate("component.maintenance_mode.message")}
-        </Typography>
-      </Stack>
+      <Card
+        sx={{
+          paddingX: 3,
+          paddingY: 4,
+          boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+        }}
+      >
+        <Stack alignItems="center" gap={8}>
+          <img width="300" src="./images/auth/maintain.svg" alt="desktop" />
+          <Stack alignItems="center" gap={2}>
+            <Typography textAlign="center" variant="h1">
+              {translate("component.maintenance_mode.title")}{" "}
+            </Typography>
+            <Typography textAlign="center" variant="h3">
+              {translate("component.maintenance_mode.message")}
+            </Typography>
+          </Stack>
+        </Stack>
+      </Card>
     </Stack>
   ) : null;
 };
